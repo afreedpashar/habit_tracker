27d357c94b1661a9accd1d6ea98a0c328bf679a8
afreed <afreedpasha1998@gmail.com>
2024-02-01 14:18:38 +0530

    habit tracking updated

---
+++ habit.py
@@ -0,0 +1,30 @@
+import requests
+
+GENDER = 'MALE'
+WEIGHT_KG = 65
+HEIGHT_CM = 170
+AGE = 25
+
+APP_ID = '295f409e'
+API_KEY = "10468863128a7ae925cc0e77de921252"
+
+exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
+
+exercise_text = input("Tell me which exercises you did: ")
+
+headers = {
+    "x-app-id": APP_ID,
+    "x-app-key": API_KEY,
+}
+
+parameters = {
+    "query": exercise_text,
+    "gender": GENDER,
+    "weight_kg": WEIGHT_KG,
+    "height_cm": HEIGHT_CM,
+    "age": AGE
+}
+
+response = requests.post(exercise_endpoint, json=parameters, headers=headers)
+result = response.json()
+print(result)
